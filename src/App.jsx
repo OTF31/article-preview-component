607de603b0604b3d7a4a9a91e9a303b3fb043ec7
denis <182906@unsaac.edu.pe>
2022-04-08 14:55:24 -0500

    Refactor initial code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <>
+      <h1 className="mt-28 text-center text-4xl font-bold text-red-900">
+        Hola Estefany perra jaaaaaaaaaa
+      </h1>
+    </>
+  );
+}
+
+export default App;
