8f84b2433614fa4639c0618b10ec1d9a07706ff1
denis <182906@unsaac.edu.pe>
2022-04-11 10:11:19 -0500

    Add font weigths and font sizes

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,16 @@ const App = () => {
           {/* <img src={Drawers} alt="Drawers" className="-z-10" /> */}
           <img src={Drawers} alt="Drawers" className="relative bottom-4" />
           <div className="absolute bottom-0 h-[310px] w-full bg-white">
-            <h1>
+            <h1 className="mx-[32.5px] mt-[38px] text-[0.99rem] font-bold leading-[24px] text-[#48556A]">
               Shift the overall look and feel by adding these wonderful touches
               to furniture in your home
             </h1>
-            <p className="">
+            <p className="mx-[32.5px] mt-[13px] text-[0.8rem] text-[#707F93]">
               Ever been in a room and felt like something was missing? Perhaps
               it felt slightly bare and uninviting. I've got some simple tips to
               help you make any room feel complete.
             </p>
-            <div className="absolute bottom-0 h-5 w-full bg-black"></div>
+            <div className="absolute bottom-0 h-[66.5px] w-full bg-slate-300"></div>
           </div>
         </div>
         {/* <div className="relative flex justify-center">
